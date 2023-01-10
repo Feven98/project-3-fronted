eacfe99eb3bc9631c6e929efbe9d1593f9cc4ccd
Feven Seifu <fevenmenelik@Fevens-MacBook-Pro.local>
2023-01-10 10:55:42 -0500

    add profile page

---
+++ src/pages/profile/profile.js
@@ -0,0 +1,33 @@
+import { useParams } from "react-router-dom"
+// import Form from "./SignUp"
+import Upload from "./ImageUpload"
+
+const ProfilePage = (props) => {
+const {id} = useParams
+
+return (
+    <div className="profile">
+ <div className="upperProfile" style={{ border: "1px solid black"}}>
+
+    <div className="profilePic" style={{ border: "1px solid black"}}>
+    <img className="pic" src="https://www.freeiconspng.com/thumbs/profile-icon-png/profile-icon-9.png" /> 
+    </div>
+    <h4></h4>
+ 
+    <h3>Total Posts:</h3>
+
+    <h3>Total Likes:</h3>
+</div>
+<div className="divider" style={{ border: "1px solid black", backgroundColor: 'cornflowerblue'}}></div>
+<div className="post" style={{ border: "1px solid black"}}> 
+
+</div>
+<h1>WORKING!!!!!</h1>
+<Upload />
+    </div>
+)
+
+
+
+}
+export default ProfilePage
