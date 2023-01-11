e771a04ebe292fa164fb34fd1d64257636fa239b
solsarr <solomonsarr18@gmail.com>
2023-01-10 21:43:01 -0500

    delete working

--- src/pages/DeleteImage.jsx
+++ src/pages/DeleteImage.jsx
@@ -2,8 +2,8 @@ import { useState } from "react"
 import { useParams } from "react-router-dom"
 import { useNavigate } from "react-router-dom"
 import '../components/App/App.css';
-import DeleteImage from "./DeleteImage";
-export default function EditImage(){
+
+export default function DeleteImage(){
 
 const navigate = useNavigate()
 
@@ -11,47 +11,31 @@ const {id} = useParams()
 
     const BASE_URL=`https://fev-sol-project3.herokuapp.com/post/${id}`
 
-    const [form, setForm] = useState({
-        image: "",
-        comment: "",
-    })
-    const handleSubmit = async (e) => {
-        // 0. prevent default (event object method)
+    const handleDelete = async (e) => {
         e.preventDefault()
-        // 1. capturing our local state
-        const currentState = { ...form }
-        console.log(currentState)
-        // check any fields for property data types / truthy value (function call - stretch)
         try {
             const requestOptions = {
-                method: "PUT",
+                method: "DELETE",
                 headers: {
                     "Content-Type": "application/json",
                 },
-                body: JSON.stringify(currentState)
             }
             const response = await fetch(BASE_URL, requestOptions)
 
             const updatedimage = await response.json()
             console.log(updatedimage)
-            navigate(`/post/${id}`)
+            navigate(`/`)
         } catch (err) {
             console.log(err)
         }
     }
-    const handleChange = (e) => {
-        // console.log(editForm)
-        const userInput = { ...form }
-        userInput[e.target.name] = e.target.value
-        setForm(userInput)
-        console.log(form)
-    }
+
 //create a form, form should accept changes for the image
 
 return (
     <>
-        <h1>Edit</h1>
-        <form onSubmit={handleSubmit}>
+        <h1>Delete</h1>
+        {/* <form onSubmit={handleDelete}>
             <br />
             <label htmlFor="Image">Image: </label>
             <input
@@ -68,12 +52,13 @@ return (
                 // value={input.password}
                 onChange={handleChange}
             />
-            <br />
-            <input type="submit" value="Confirm Edit" />
+            <br /> */}
+            {/* <input type="submit" value="DELETE POST" /> */}
+            <button onClick={handleDelete}>DELETE POST</button>
 
-        </form>
-        <img src={form.image} />
-        <DeleteImage />
+        {/* </form> */}
+        {/* <img src={form.image} /> */}
+
     </>
 )
 
